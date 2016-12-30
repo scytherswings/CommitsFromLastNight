3ec33a212c5484917785f018d1b5cf36da310167
scytherswings <scytherswings@gmail.com>
2016-12-29 22:30:13 -0500

    fiddling with filterset

--- app/models/filterset.rb
+++ app/models/filterset.rb
@@ -4,24 +4,22 @@ class Filterset < ActiveRecord::Base
   has_many :commits, through: :filtered_messages
 
   def execute(commit)
-    
+
   end
 
   def match_black_list_words(commit)
-   commit.message.split(/s/).each  do |message_word|
-    black_list_words.each do |word|
-      
-      
-      
-      
-      
-      
+    commit.message.split(/\W+/).each do |message_word|
+      black_list_words.each do |word|
+
+
+      end
+    end
+
   end
 
   def match_white_list_words
 
   end
-
 
 
 end
