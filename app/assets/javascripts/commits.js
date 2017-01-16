aa522881db18469dca7c53ae61b938266591f0d7
scytherswings <scytherswings@gmail.com>
2017-01-16 00:45:35 -0500

    added simple_form_bootstrap apparently. Fiddling with multiselect and ways to get requests to the server

--- app/assets/javascripts/commits.js
+++ app/assets/javascripts/commits.js
@@ -1,6 +1,24 @@
 $(function () {
-    if ($("#multiselect_commits").length) {
-        console.log("Fuck javascript")
+    let multiselect = $("#multiselect_commits");
+    if (multiselect.length) {
+        $(multiselect).change(function () {
+        let url_params = [];
+        $(':selected').each(function (i, selected) {
+            let selected_item = $(selected);
+            console.log('Selected item: ' + selected_item.attr('id'));
+            url_params.push(selected_item.attr('id'))
+        });
+        console.log(url_params);
+        let queryParameters = {};
+        // let queryParameters = {}, queryString = location.search.substring(1),
+        //     re = /([^&=]+)=([^&]*)/g, m;
+        //
+        // while (m = re.exec(queryString)) {
+        //     queryParameters[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+        // }
+        queryParameters['categories'] = url_params;
+        // location.search = $.param(queryParameters); // Causes page to reload
+        });
     }
 });
 
