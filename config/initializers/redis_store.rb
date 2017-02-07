c40c8667665155bc6bb995e9e94c07f15934caa3
scytherswings <scytherswings@gmail.com>
2017-02-07 00:59:21 -0500

    Fixed redis usage to separate caches on environment.

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,1 +1,1 @@
-CommitsFromLastNight::Application.config.cache_store = :redis_store, 'redis://127.0.0.1:6379/1/cache'
+CommitsFromLastNight::Application.config.cache_store = :redis_store, "redis://127.0.0.1:6379/0/#{Rails.env}/cache"
