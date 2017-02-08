797587685bceaae5b7bb8b4d8111db7c4a7a8bea
scytherswings <scytherswings@gmail.com>
2017-02-08 01:27:39 -0500

    Hoooooly fuck. FUCK JAVASCRIPT THIS IS THE DEVIL'S LANGUAGE. I swear it was designed by post-lobotomy patients and monkeys. Uck I can't understand how anyone would *want* to choose to work in this language. Everything is completly fucky. /rage

--- app/assets/javascripts/commits.js
+++ app/assets/javascripts/commits.js
@@ -1,10 +1,56 @@
-// function highlight_words() {
-//     $('[id^=commit_message_id]').highlight(['fuck', 'suck', 'damn', 'shit']);
-//     console.log("Words have been highlighted.");
-// }
-// $(document).ready(highlight_words);
-// $(document).on('page:load', highlight_words);
-//
+let categories = {};
+
+function getListOfSelectedCategories() {
+    return $.map($('#multiselect_commits').find(':selected'), function (obj) {
+        return obj.value;
+    })
+}
+
+function highlight_text(keywords) {
+    if (typeof keywords !== 'undefined' && keywords.length > 0){
+        $('[id^=commit_message_id]').highlight(keywords);
+    }
+}
+
+function getListOfFilterWordsAndHighlight(listOfCategories) {
+    console.log(listOfCategories);
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/highlight_keywords",
+        data: {'categories':listOfCategories},
+        success: function (response) {
+            console.log(response);
+            categories['categories'] = response.categories;
+            categories['keywords'] = response.keywords;
+            highlight_text(categories['keywords']);
+            enableHighlighting();
+        },
+        error: function (response, status, error) {
+            $("#error_modal_body").html(error);
+            $("#error_modal").modal("show");
+        }
+    });
+}
+
+function disableHighlighting() {
+    $('mark.hoverable').css('background', 'none');
+    console.log("Keywords should not be highlighted now.")
+}
+
+function enableHighlighting() {
+    $('mark.hoverable').css('background', 'lemonchiffon');
+    console.log("Keywords should be highlighted now.")
+}
+
+jQuery.changeHighlighting = function () {
+    if ($('#HighlightToggle').is(':checked')) {
+        highlight_text(categories['keywords']);
+        enableHighlighting();
+    } else {
+        disableHighlighting();
+    }
+};
 
 function refreshWithSelectedCategories() {
     let url_params = new Set;
@@ -25,33 +71,5 @@ function refreshWithSelectedCategories() {
     location.search = $.param(url_params);
 }
 
-jQuery.toggleHighlighting = function () {
-    changeHighlighting($('#HighlightToggle'))
-};
-
-jQuery.runToggleOnChange = function runToggleOnChange() {
-    $('#HighlightToggle').change(function () {
-        changeHighlighting($('#HighlightToggle'));
-    })
-};
-
-function changeHighlighting(highlight_toggle) {
-    if (highlight_toggle.is(':checked')) {
-        enableHighlighting();
-    } else {
-        disableHighlighting();
-    }
-}
-
-function enableHighlighting() {
-    $('mark.hoverable').css('background', 'lemonchiffon');
-    console.log("Keywords should be highlighted now.")
-}
-
-function disableHighlighting() {
-    $('mark.hoverable').css('background', 'none');
-    console.log("Keywords should not be highlighted now.")
-}
-
-$(document).ready($.toggleHighlighting);
-$(document).on('page:load', $.toggleHighlighting);
+$(document).ready($.changeHighlighting);
+$(document).on('page:load', $.changeHighlighting);
