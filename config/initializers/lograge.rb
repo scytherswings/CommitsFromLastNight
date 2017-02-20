8aac80178c963b2555a12e25a64f898b106c8d83
scytherswings <scytherswings@gmail.com>
2017-02-20 15:43:05 -0500

    Added params hash to lograge output

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,9 @@
 CommitsFromLastNight::Application.configure do
   config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w(controller action format id)
+    {
+        params: event.payload[:params].except(*exceptions)
+    }
+  end
 end
