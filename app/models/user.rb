257b55124af2ef764e98feb1d2670f41e999345d
scytherswings <scytherswings@gmail.com>
2017-02-20 00:39:03 -0500

    removed branch_name from commits. Tweaked validator for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-  include ArelHelpers::ArelTable
   has_many :commits, dependent: :destroy
   has_many :email_addresses, dependent: :destroy
   has_many :repositories, through: :commits
 
-  validates_presence_of :account_name, unique: true
+  validates_uniqueness_of :account_name
 end
