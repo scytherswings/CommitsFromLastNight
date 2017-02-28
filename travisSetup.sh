ab57b975ec14d3489af6f782abe3817aec8feea8
scytherswings <scytherswings@gmail.com>
2017-02-27 22:04:10 -0500

    Make sure rails environment is test

--- travisSetup.sh
+++ travisSetup.sh
@@ -8,6 +8,7 @@ echo -e "test:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 touch "config.yml"
 echo -e "username: " $BB_USERNAME "\n" >> "config.yml"
 echo -e "password: " $BB_PASSWORD "\n" >> "config.yml"
+export RAILS_ENV=test
 bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
