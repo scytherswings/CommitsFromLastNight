73be73f7191bbacd7573d010227e1957c9f43c42
scytherswings <scytherswings@gmail.com>
2017-02-27 22:02:15 -0500

    adding env variable username and password for testing

--- travisSetup.sh
+++ travisSetup.sh
@@ -5,5 +5,9 @@ BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # rm $SECRETS
 echo -e "test:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 # rm db/*sqlite3
-rake db:create
-rake db:migrate
+touch "config.yml"
+echo -e "username: " $BB_USERNAME "\n" >> "config.yml"
+echo -e "password: " $BB_PASSWORD "\n" >> "config.yml"
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
