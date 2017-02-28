d4913750d5d49525b599f8643640fa6bb516c4c2
scytherswings <scytherswings@gmail.com>
2017-02-27 21:46:52 -0500

    bundle update maybe

--- travisSetup.sh
+++ travisSetup.sh
@@ -3,9 +3,7 @@ SECRETS="config/secrets.yml"
 SECRET="$(bundle exec rake secret)"
 BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # rm $SECRETS
-echo -e "development:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
-echo -e "production:\n secret_key_base: " $SECRET >> $SECRETS
 # rm db/*sqlite3
 rake db:create
 rake db:migrate
