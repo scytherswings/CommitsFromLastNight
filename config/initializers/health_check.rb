c9a2f0ae911a10ec596a4ce1f07750f086f8963f
scytherswings <scytherswings@gmail.com>
2017-03-11 14:40:05 -0500

    remove sidekiq from checks since it doesn't run on the webserver

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -27,7 +27,7 @@ HealthCheck.setup do |config|
   config.standard_checks = %w(database cache)
 
   # You can set what tests are run with the 'full' or 'all' parameter
-  config.full_checks = %w(database migrations cache redis sidekiq-redis)
+  config.full_checks = %w(database migrations cache redis)
 
   # max-age of response in seconds
   # cache-control is public when max_age > 1 and basic_auth_username is not set
