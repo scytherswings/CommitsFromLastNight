927fc9c424fd0ec420af741421058dfa6f8ab1cc
scytherswings <scytherswings@gmail.com>
2017-03-11 13:52:49 -0500

    fixed redis urls

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,1 +1,5 @@
-CommitsFromLastNight::Application.config.cache_store = :redis_store, "redis://127.0.0.1:6379/0/#{Rails.env}/cache"
+redis_config = YAML.load_file(Rails.root.join 'config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+
+CommitsFromLastNight::Application.config.cache_store = :redis_store, "redis://#{redis_config[:host]}:#{redis_config[:port]}/0/#{Rails.env}/cache"
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,10 @@ redis_config = YAML.load_file(Rails.root.join 'config/redis.yml')
 redis_config.merge! redis_config.fetch(Rails.env, {})
 redis_config.symbolize_keys!
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0#{Rails.env}" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0/sidekiq/#{Rails.env}" }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0/#{Rails.env}" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0/sidekiq/#{Rails.env}" }
 end
 
 if Rails.env == 'production'
