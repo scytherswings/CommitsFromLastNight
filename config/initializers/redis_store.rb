7f49e72fe181912056e0c34eaf499de0a4895dc8
scytherswings <scytherswings@gmail.com>
2017-03-11 16:40:17 -0500

    fixed redis uris

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,5 +1,7 @@
-redis_config = YAML.load_file(Rails.root.join 'config/redis-store.yml')
-redis_config.merge! redis_config.fetch(Rails.env, {})
-redis_config.symbolize_keys!
-
-CommitsFromLastNight::Application.config.cache_store = :redis_store, "redis://#{redis_config[:uri]}/0/#{Rails.env}/cache"
+CommitsFromLastNight::Application.config do |config|
+  if Rails.env == 'production'
+      config.cache_store = :redis_store, "redis://#{ENV['REDIS_STORE_URI']}/0/#{Rails.env}/cache"
+  else
+    config.cache_store = :redis_store, "redis://127.0.0.1:6379/0/#{Rails.env}/cache"
+  end
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,19 @@
 Sidekiq::Logging.logger = Logger.new("log/sidekiq_#{Rails.env}.log")
 
-redis_config = YAML.load_file(Rails.root.join 'config/sidekiq-redis.yml')
-redis_config.merge! redis_config.fetch(Rails.env, {})
-redis_config.symbolize_keys!
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:uri]}/0/sidekiq/#{Rails.env}" }
-end
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:uri]}/0/sidekiq/#{Rails.env}" }
+if Rails.env == 'production'
+  Sidekiq.configure_server do |config|
+    config.redis = {url: "redis://#{ENV['SIDEKIQ_REDIS_URI']}/0/#{Rails.env}/sidekiq"}
+  end
+  Sidekiq.configure_client do |config|
+    config.redis = {url: "redis://#{ENV['SIDEKIQ_REDIS_URI']}/0/#{Rails.env}/sidekiq"}
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.redis = {url: "redis://127.0.0.1:6379/0/#{Rails.env}/sidekiq"}
+  end
+  Sidekiq.configure_client do |config|
+    config.redis = {url: "redis://127.0.0.1:6379/0/#{Rails.env}/sidekiq"}
+  end
 end
 
 if Rails.env == 'production'
