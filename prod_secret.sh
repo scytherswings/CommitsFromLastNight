7a006cf27339ce6b6c34c83f80e97fb4d549698e
scytherswings <scytherswings@gmail.com>
2017-03-11 13:37:57 -0500

    ok let's try to make it generate the secret now

--- prod_secret.sh
+++ prod_secret.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-SECRETS="config/secrets.yml"
 SECRET="$(bundle exec rake secret)"
-rm $SECRETS
-touch $SECRETS
-echo -e "production:\n  secret_key_base: " $SECRET "\n" >> $SECRETS
+export SECRET_KEY_BASE=$SECRET
