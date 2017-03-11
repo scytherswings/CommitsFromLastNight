2034936a4ffd7352836f8ecddae9bfd93b9774d0
scytherswings <scytherswings@gmail.com>
2017-03-11 13:31:13 -0500

    does it even need to fool with secrets?

--- travisSetup.sh
+++ travisSetup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-SECRETS="config/secrets.yml"
-SECRET="$(bundle exec rake secret)"
-BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-# rm $SECRETS
-echo -e "test:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 touch "config.yml"
 echo -e "username: " $BB_USERNAME "\n" >> "config.yml"
 echo -e "password: " $BB_PASSWORD "\n" >> "config.yml"
