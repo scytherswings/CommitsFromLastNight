ddc3edb72026ddcada419896cf3f8ada0c331c38
scytherswings <scytherswings@gmail.com>
2017-03-11 03:01:57 -0500

    fucking around with stage configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,16 @@
 Sidekiq::Logging.logger = Logger.new("log/sidekiq_#{Rails.env}.log")
 
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
 Sidekiq.configure_server do |config|
-  config.redis = {url: "redis://127.0.0.1:6379/0/#{Rails.env}"}
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0#{Rails.env}" }
 end
-
 Sidekiq.configure_client do |config|
-  config.redis = {url: "redis://127.0.0.1:6379/0/#{Rails.env}"}
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0/#{Rails.env}" }
 end
 
 if Rails.env == 'production'
-  Sidekiq::Logging.logger.level = Logger::WARN
+  Sidekiq::Logging.logger.level = Logger::INFO
 end
-
