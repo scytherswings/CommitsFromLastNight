db6ba07cb00f5921c2b631cdf9cc5a3c7386c825
scytherswings <scytherswings@gmail.com>
2017-03-11 17:50:39 -0500

    reformatted redischecker

--- lib/healthchecks/redis_checker.rb
+++ lib/healthchecks/redis_checker.rb
@@ -1,18 +1,17 @@
 require 'health_check'
+class RedisChecker
+  extend BaseHealthCheck
 
-  class RedisChecker
-    extend BaseHealthCheck
-
-    def self.check(redis_instance_type, redis_uri)
-      begin
-        unless defined?(::Redis)
-          raise "Wrong configuration. Missing 'redis' gem"
-        end
-        res = ::Redis.new(url: redis_uri).ping
-        res == 'PONG' ? '' : "Redis.ping returned #{res.inspect} instead of PONG"
-      rescue Exception => e
-        create_error(redis_instance_type, e.message)
+  def self.check(redis_instance_type, redis_uri)
+    begin
+      unless defined?(::Redis)
+        raise "Wrong configuration. Missing 'redis' gem"
       end
+      res = ::Redis.new(url: redis_uri).ping
+      res == 'PONG' ? '' : "Redis.ping returned #{res.inspect} instead of PONG"
+    rescue Exception => e
+      create_error(redis_instance_type, e.message)
     end
   end
+end
 
