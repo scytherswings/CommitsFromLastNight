5a1c372aa2e4437c84370852077a10d29d69d320
scytherswings <scytherswings@gmail.com>
2017-03-15 02:12:04 -0400

    made redis-browser config

--- config/initializers/redis-browser.rb
+++ config/initializers/redis-browser.rb
@@ -1,5 +1,10 @@
+require 'redis-browser'
 RedisBrowser::Web.class_eval do
   use Rack::Auth::Basic, 'Administrator area' do |username, password|
     username == 'admin' && password == ENV['ADMIN_PASSWORD']
   end
+  config = Rails.root.join('config', 'redis-browser.yml')
+  settings = YAML.load(ERB.new(IO.read(config)).result)
+  RedisBrowser.configure(settings)
 end
+
