a57e7b5cbba958dea1847d9293925161ceacea4d
scytherswings <scytherswings@gmail.com>
2017-03-15 00:03:24 -0400

    fixed seeds so it doesn't rely on faker by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,7 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
+
 require 'importers/filter'
 
 filter_files = Dir.glob('.resources/filter_categories/*.yml')
@@ -14,22 +13,24 @@ filter_files.each do |file|
   Importers::Filter.new.create_filterset_from_file(file)
 end
 
-
-def generate_random_interjection
-  ['..damn..', '..fuck!', '..well that makes me sad.', '..how disappointing.', '...yes!', '...oh no..', '..whooppee!',
-   '...holy shit!', 'gee willikers, Batman!',
-   "...why do people use python anyway? There's no way it could be as fun as Ruby!"].sample
-end
-
-def generate_random_message
-  message = []
-  message << Faker::Hacker.say_something_smart
-  message << (rand(0..1).times.collect { |_| generate_random_interjection }).join('')
-  message << (rand(0..1).times.collect { |_| Faker::Hacker.say_something_smart }).join('')
-  message.join(' ').strip
-end
+#
+# def generate_random_interjection
+#   ['..damn..', '..fuck!', '..well that makes me sad.', '..how disappointing.', '...yes!', '...oh no..', '..whooppee!',
+#    '...holy shit!', 'gee willikers, Batman!',
+#    "...why do people use python anyway? There's no way it could be as fun as Ruby!"].sample
+# end
+#
+# def generate_random_message
+#   #   Mayor.create(name: 'Emanuel', city: cities.first)
+#   message = []
+#   message << Faker::Hacker.say_something_smart
+#   message << (rand(0..1).times.collect { |_| generate_random_interjection }).join('')
+#   message << (rand(0..1).times.collect { |_| Faker::Hacker.say_something_smart }).join('')
+#   message.join(' ').strip
+# end
 #
 # if Rails.env == 'test'
+#   require 'faker'
 #   account_names = Array.new
 #   repository_names = Array.new
 #
